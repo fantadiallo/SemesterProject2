2a35e95fe71f2ddfeb47225b40d95463421491f3
fantadiallo <fatmbo0003@outlook.com>
2025-04-20 23:01:58 +0200

    Fix: wrap top-level await in async function to support Netlify build

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import router from "./js/router/index.js";
 import { showLogoutLink } from './js/ui/components/showlogout.js';
 import { onLogout } from './js/ui/auth/logout.js';
 
-await router(window.location.pathname);
-console.log("JS is working ✅");
-showLogoutLink();
-const logoutLink = document.getElementById("logoutLink");
-if (logoutLink) {
-  logoutLink.addEventListener("click", onLogout);
-}
+(async () => {
+  await router(window.location.pathname);
+  console.log("JS is working ✅");
+  showLogoutLink();
 
-
+  const logoutLink = document.getElementById("logoutLink");
+  if (logoutLink) {
+    logoutLink.addEventListener("click", onLogout);
+  }
+})();
 
